0ac7bec8b72401d1f6a14cd5d59a20162f232dd8
Kmberry1989 <kylematthewberry@gmail.com>
2025-09-09 00:08:49 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,10 +7,9 @@ const nextConfig = {
     // Allow loading images from the public directory and remote domains if required
     domains: [],
   },
-  // Some three.js modules ship as untranspiled ES modules.  By
+  // Some three.js modules ship as untranspiled ES modules. By
   // specifying `three` here, Next.js will transpile it when
-  // building to avoid errors on older browsers【162625553879168†L46-L67】.
+  // building to avoid errors on older browsers.
   transpilePackages: ['three'],
 };
-
 module.exports = nextConfig;
